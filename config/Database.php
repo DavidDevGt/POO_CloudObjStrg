9a5422565077f99f6d71b43501332031416edc31
DavidDevGt <josuedavidvl18@gmail.com>
2024-01-11 20:59:51 -0600

    first commit

---
+++ config/Database.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Config;
+
+use PDO;
+use PDOException;
+
+class Database {
+    private $host = "localhost";
+    private $db_name = "pdf_store";
+    private $username = "root";
+    private $password = "";
+    private $conn;
+
+    public function getDBConection() {
+        $this->conn = null;
+        try {
+            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
+            $this->conn->exec("set names utf8");
+        } catch (PDOException $exception) {
+            echo "Error de conexión: " . $exception->getMessage();
+        }
+        return $this->conn;
+    }
+}

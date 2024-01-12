1abeda26dcd254e267785b7782aa135e417b10fd
DavidDevGt <josuedavidvl18@gmail.com>
2024-01-11 22:12:28 -0600

    Add file upload validation and error handling

--- models/Upload.php
+++ models/Upload.php
@@ -14,6 +14,19 @@ class Upload {
 
     public function uploadFile($file) {
         // TODO - Upload file to server
+        $targetDir = "../uploads/";
+        $targetFile = $targetDir . basename($file["name"]);
+        $fileType = strtolower(pathinfo($targetFile, PATHINFO_EXTENSION));
+
+        // Revisar si el archivo es un PDF
+        if ($fileType != "pdf") {
+            throw new \Exception("El archivo que quieres subir no es un PDF");
+        }
+
+        // Revisar si el archivo ya existe
+        if (file_exists($targetFile)) {
+            throw new \Exception("El archivo que quieres subir ya existe");
+        }
     }
 
     public function saveMetadata() {

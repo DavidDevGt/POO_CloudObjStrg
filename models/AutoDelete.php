dee8b306c641ad15b70dd15e3f4c3a7b029137dc
DavidDevGt <josuedavidvl18@gmail.com>
2024-01-13 08:47:18 -0600

    Add AutoDelete class to delete expired documents and links

--- models/AutoDelete.php
+++ models/AutoDelete.php
@@ -2,5 +2,27 @@
 
 namespace Models;
 
+use Config\Database;
 use Exception;
 use PDOException;
+
+class AutoDelete
+{
+    private $db;
+
+    public function __construct()
+    {
+        $database = new Database();
+        $this->db = $database->getDBConection();
+    }
+
+    public function deleteExpiredDocuments()
+    {
+        try {
+            $this->db->exec("UPDATE documentos SET active = FALSE WHERE fecha_subida < NOW() - INTERVAL 12 HOUR");
+            $this->db->exec("UPDATE enlaces_cortos SET active = FALSE WHERE fecha_expiracion < NOW()");
+        } catch (PDOException $e) {
+            throw new Exception("Error al eliminar documentos o enlaces caducados: " . $e->getMessage());
+        }
+    }
+}

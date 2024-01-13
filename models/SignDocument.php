1f2846af6f65b7ee219432ccaa187aa313341fdc
DavidDevGt <josuedavidvl18@gmail.com>
2024-01-13 08:39:38 -0600

    Refactor SignDocument class and add saveSignature method

--- models/SignDocument.php
+++ models/SignDocument.php
@@ -6,6 +6,25 @@ use Config\Database;
 use Exception;
 use PDOException;
 
-class SignDocument {
+class SignDocument
+{
     private $db;
+
+    public function __construct()
+    {
+        $database = new Database();
+        $this->db = $database->getDBConection();
+    }
+
+    public function saveSignature($documentoId, $firmaData)
+    {
+        try {
+            $stmt = $this->db->prepare("INSERT INTO firmas (documento_id, firma_data) VALUES (:documento_id, :firma_data)");
+            $stmt->bindParam(":documento_id", $documentoId);
+            $stmt->bindParam(":firma_data", $firmaData);
+            $stmt->execute();
+        } catch (PDOException $e) {
+            throw new Exception("Error al guardar la firma: " . $e->getMessage());
+        }
+    }
 }
